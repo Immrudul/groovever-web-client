82d7ef6c3d6c6e95ef0d8264fed012fcfeb7f5ec
Mrudul Suresh <mail2mrudul@gmail.com>
2024-03-20 18:48:41 -0400

    top connect page

--- src/app/connect/page.tsx
+++ src/app/connect/page.tsx
@@ -1,7 +1,36 @@
+import Image from "next/image";
+
 export default function ConnectPage() {
   return (
     <div className="">
-      <div className=""></div>
+      <div className="relative h-[60vh] lg:h-[75vh] overflow-hidden">
+        <div className="h-full absolute inset-0">
+          <div className="relative h-full">
+            <Image
+              src="/Dreamville.jpg"
+              alt="learn"
+              style={{ objectFit: "cover", opacity: 0.5 }} // Adjust opacity as needed
+              priority
+              fill
+            />
+            <div className="absolute inset-0 flex flex-col items-center justify-center">
+              <div className="text-gorange text-6xl lg:text-8xl font-black">
+                CONNECT
+              </div>
+              <br />
+              <br />
+              <div className="text-center text-lg lg:text-xl">
+                Find dancers in your city <b>locally</b> or around the world{" "}
+                <b>globally</b>!
+              </div>
+              <br />
+              <div className="text-center text-lg lg:text-xl">
+                Dance with peers to gain motivation and learn from each other.
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
