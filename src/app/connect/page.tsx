496d6d0c7208d1b29cdae88b1ee721787fcb5ae7
Mrudul Suresh <mail2mrudul@gmail.com>
2024-03-21 12:45:53 -0400

    finished connect card + mobile responsiveness

--- src/app/connect/page.tsx
+++ src/app/connect/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function ConnectPage() {
   return (
     <div className="">
-      <div className="relative h-[60vh] lg:h-[75vh] overflow-hidden">
+      <div className="relative h-[60vh] xl:h-[75vh] overflow-hidden">
         <div className="h-full absolute inset-0">
           <div className="relative h-full">
             <Image
@@ -14,17 +14,17 @@ export default function ConnectPage() {
               fill
             />
             <div className="absolute inset-0 flex flex-col items-center justify-center">
-              <div className="text-gorange text-6xl lg:text-8xl font-black">
+              <div className="text-gorange text-6xl xl:text-8xl font-black">
                 CONNECT
               </div>
               <br />
               <br />
-              <div className="text-center text-lg lg:text-xl">
+              <div className="text-center text-xl xl:text-xl">
                 Find dancers in your city <b>locally</b> or around the world{" "}
                 <b>globally</b>!
               </div>
               <br />
-              <div className="text-center text-lg lg:text-xl">
+              <div className="text-center text-xl xl:text-xl">
                 Dance with peers to gain motivation and learn from each other.
               </div>
             </div>
@@ -32,15 +32,34 @@ export default function ConnectPage() {
         </div>
       </div>
 
-      <div className="join flex lg:flex-row flex-col border-8 mx-10 rounded-3xl border-orange-400 my-10">
-        <div className="lg:w-1/3">
+      <div className="join flex xl:flex-row flex-col xl:border-8 border-4 mx-10 xl:rounded-3xl rounded-2xl border-orange-400 my-10">
+        <div className="xl:w-1/3">
           <img
             src="V.png"
             alt="Album"
-            className="aspect-video object-cover rounded-2xl w-full"
+            className=" object-cover xl:rounded-2xl rounded-xl w-full aspect-video"
           />
         </div>
-        <div className="bg-white lg:-ml-12 ">hello lol</div>
+        {/* Dont remove the 0.1px padding it actually does make a difference in the mobile version believe it or not! */}
+        <div className="bg-white xl:h-auto flex flex-col justify-between xl:p-6 xl:-ml-12 xl:mx-0 p-5 xl:my-0 mb-10 -mt-3 ml-[0.1px] xl:w-2/3 ">
+          <div className="">
+            <div className="text-4xl font-black pb-4">
+              University of Waterloo Street Dance
+            </div>
+            <div className="xl:-mr-10 text-lg">
+              This is the place holder for the content of UofW street dance
+              introduction, and it's good to know that Elysia is my waifu! This
+              is the place holder for the content of UofW street dance
+              introduction, and it's good to know that Elysia is my waifu! This
+              is the place holder for the content of UofW street dance
+              introduction, and it's good to know that Elysia is my waifu!
+            </div>
+          </div>
+          <div className="space-x-2 xl:mt-0 mt-4 xl:mb-0 -mb-10 text-lg">
+            <span>FRIENDLY LINKS:</span>
+            <span>Website Website Website Website</span>
+          </div>
+        </div>
       </div>
     </div>
   );
